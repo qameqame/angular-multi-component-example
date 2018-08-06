48ff35c64c96128098cfc77110b22b3ccf99db66
kameyama <kameyama@kameyama-no-MacBook-Pro.local>
2018-08-06 17:32:22 +0900

    add material dialog sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { OrderEditComponent } from './order-edit/order-edit.component';
+import { MatDialogModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { OrderInputComponent } from './order-input/order-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderListComponent,
+    OrderEditComponent,
+    OrderInputComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+  ],
+  entryComponents: [
+    OrderInputComponent
+  ],
 })
 export class AppModule { }
